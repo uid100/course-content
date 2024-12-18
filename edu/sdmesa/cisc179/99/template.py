21fdb7adb07ed55d4475987a9eb5fcac8c599879
Dan Sullivan <dev@officewires.com>
2024-12-17 17:49:02 -0800

    CISC 179 planning notes

---
+++ edu/sdmesa/cisc179/99/template.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+File name: template.py
+Author: Your Name Here!
+Created: 2025-Jan-01
+Version: 1.0
+Description: A sample Python program
+"""
+
+print("Hello, World!")
