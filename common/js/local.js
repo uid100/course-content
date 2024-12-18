aa331cd2c10245970a04985160c1476890c9e24c
Dan Sullivan <dev@officewires.com>
2024-12-18 14:59:32 -0800

    add link to college

--- common/js/local.js
+++ common/js/local.js
@@ -13,7 +13,9 @@ document.addEventListener("DOMContentLoaded", () => {
         .then(data => {
             // Extract the logo path from the JSON
             const logoPath = data?.college?.logoPath;
+            const website = data?.college?.website;
 
+            // set the logo image source
             if (logoPath) {
                 // Replace `~` with the base path if necessary
                 const relativePath = logoPath.replace(/^~\//, "");
@@ -23,6 +25,15 @@ document.addEventListener("DOMContentLoaded", () => {
                 imgElement.src = relativePath;
             } else {
                 console.error("Logo path not found in JSON.");
+            }
+
+            // set the website link
+            if (website) {
+                // Set the href attribute of the link
+                const linkElement = document.getElementById("college-link");
+                linkElement.href = website;
+            } else {
+                console.error("Website link not found in JSON.");
             }
         })
         .catch(error => {
