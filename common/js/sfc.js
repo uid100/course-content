40e5702ac2d009c48e3da7820b5e28d101d6d8cd
Dan Sullivan <dev@officewires.com>
2025-01-03 18:36:27 -0800

    Update sfc.js

--- common/js/sfc.js
+++ common/js/sfc.js
@@ -44,10 +44,10 @@ async function loadCourseDetails() {
         const data = await response.json();
 
         // Populate table cells with JSON data
+        document.getElementById('courseTitle').textContent += data?.course?.courseTitle;
         document.getElementById('schedule').textContent = data?.course?.section?.schedule?.day
             + " " + data.course?.section?.schedule?.time;
         document.getElementById('location').textContent = data?.course?.section?.schedule?.location;
-        document.getElementById('courseTitle').textContent = data?.course?.courseTitle;
         document.getElementById('instructorName').textContent = data?.instructor?.firstName
             + " " + data.instructor?.lastName;
         document.getElementById('instructorEmail').textContent = data?.instructor?.email;
