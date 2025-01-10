06dd8a2ac2178b14e0d5b5144a930d24af23ee2f
Dan Sullivan <dev@officewires.com>
2025-01-10 14:55:36 -0800

    fix timezone issue with timeline

---
+++ edu/palomar/test.js
@@ -0,0 +1,6 @@
+const start = new Date("2025-04-10T08:00:00");
+
+const today = new Date();
+
+console.log("today " + today.toLocaleDateString());
+console.log("start " + start.toLocaleDateString());
