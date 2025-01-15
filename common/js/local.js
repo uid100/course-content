071d4c2f6879e46a7f1ee3767070a7604e7ed21f
Dan Sullivan <dev@officewires.com>
2025-01-15 10:35:18 -0800

    fix zoom links

--- common/js/local.js
+++ common/js/local.js
@@ -292,7 +292,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
             // office hours
             const officeHoursButton = document.getElementById("office-hours-button");
-            console.log("office hours" + officeHoursLink);
+            console.log("office hours: " + officeHoursLink);
             if (officeHoursButton) {
                 officeHoursButton.textContent = officeHours;
                 officeHoursButton.href = officeHoursLink;
