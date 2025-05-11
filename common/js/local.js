e2b97c2662690665d41383151b7075ad8b4beea0
Dan Sullivan <dev@officewires.com>
2025-05-11 02:12:12 -0700

    Update local.js

--- common/js/local.js
+++ common/js/local.js
@@ -187,7 +187,6 @@ document.addEventListener("DOMContentLoaded", () => {
             const objectives = data?.course?.objectives;
             console.log("module-num: " + moduleNum);
             console.log("moduleNum type: ", typeof moduleNum);
-            console.log("First module value: ", data.modules[0].module, " type: ", typeof data.course.modules[0].module);
             const moduleName = data?.modules?.find(m => String(m.module) === String(moduleNum))?.title;
             console.log("module-name: " + moduleName);
             const outcomes = data?.course?.outcomes;
